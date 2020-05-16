4377e414c61734d9faba92396da605afa7f58271
MP61592WSB <59970246+MP61592WSB@users.noreply.github.com>
2020-05-16 07:46:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
 var time = 0;
+
 $("document").ready(
     function(evt)
     {
+        start();   
+        
         $("#shoutbox").submit(
             function()
             {
-                $.post("odbierz_dane.php", {
-                    "text": $("#text").val(),
-                    "nick": $("#nick").val(),
-                    "time": time,
-                    "action": "post"
-                },
-                function(daneXML)
-                {
-                    addMessage(daneXML);
-                });
+                
+                $("#error").css({"color": "red"});
+
+                if($("#text").val() == "" && $("#nick").val() == "")
+                    {
+                        document.getElementById("error").innerHTML = "*You must enter a text message nad nickname!";
+                    }
+                
+                else if($("#text").val() != "" && $("#nick").val() == "")
+                    {
+                            document.getElementById("error").innerHTML = "*You must enter a nickname!";
+                    }
+                else if($("#text").val() == "" && $("#nick").val() != "")
+                    {
+                            document.getElementById("error").innerHTML = "*You must enter a text message!";
+                    }
+                else if($("#text").val() != "" && $("#nick").val() != "")
+                    {
+                        
+                        $.post("odbierz_dane.php", {
+                            "text": $("#text").val(),
+                            "nick": $("#nick").val(),
+                            "time": time,
+                            "action": "post"
+                        },
+                        function(daneXML)
+                        {
+                            document.getElementById("messages").innerHTML = "";
+                            addMessage(daneXML);
+                        });
+                    }
+                else
+                    {
+                        document.getElementById("messages").innerHTML = "";
+                    }
                 
                 $("#text").val('');
                 return false;
             }
         );
+        
         updateMessage();
     }
 );
@@ -35,9 +64,9 @@ function updateMessage()
     
     setTimeout(updateMessage, 2000);
 }
-
 function addMessage(daneXML)
 {
+       
     if($("status", daneXML).text() == "0") return;
     
     var wiadomosci = $("wiadomosc", daneXML);
@@ -49,8 +78,30 @@ function addMessage(daneXML)
             var nick = $("nick", this);
             var tresc = $("tresc", this);
             var czas = $("czas", this);
-            
             $("#messages").prepend("["+czas.text()+"] [<b>"+nick.text()+"</b>] "+tresc.text()+"</br>");
+            
         }
+    );
+}
+
+function start()
+{
+     $.post("view.php",
+        function(daneXML)
+        {
+
+            var wiadomosci = $("wiadomosc", daneXML);
+
+            wiadomosci.each(
+                function(i)
+                {
+                    var nick = $("nick", this);
+                    var tresc = $("tresc", this);
+                    var czas = $("czas", this);
+                    $("#messages").prepend("["+czas.text()+"] [<b>"+nick.text()+"</b>] "+tresc.text()+"</br>");
+
+                }
+            );
+        }      
     );
 }
